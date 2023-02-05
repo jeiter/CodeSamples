6b0dc2f2fd1ea86e2a126e2e0973aad2acad5a26
Josef Eiter (Privat) <joeiprivat@Josefs-MBP.fritz.box>
2023-02-05 12:03:17 +0100

    Add DeconstructObjects codingsample

---
+++ DeconstructObjects/DeconstructObjescs/Location.cs
@@ -0,0 +1,14 @@
+﻿namespace DeconstructObjects
+{
+	public class Location
+	{
+		public double Latitude { get; set; }
+		public double Longitude { get; set; }
+
+		public void Deconstruct(out double latitude, out double longitude)
+		{
+			latitude = Latitude;
+			longitude = Longitude;
+		}
+	}
+}
---
+++ DeconstructObjects/DeconstructObjescs/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace DeconstructObjects {
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var location = new Location
+            {
+                Latitude = 52.5141156,
+                Longitude = 13.2857691,
+            };
+
+            (double latitude, double longitude) = location;
+            Console.WriteLine($"lat: {latitude}, long: {longitude}"); // lat: 52.5141156, long: 13.2857691
+        }
+    }
+}
