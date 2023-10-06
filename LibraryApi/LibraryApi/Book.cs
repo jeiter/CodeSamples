2cd5e50532e2bad1f0d77e0691250ed34c8cd47b
jeiter <eiter.josef@googlemail.com>
2023-10-06 19:07:40 +0200

    Renaming and changing models

---
+++ LibraryApi/LibraryApi/Book.cs
@@ -0,0 +1,16 @@
+﻿namespace LibraryApi;
+
+public class Book
+{
+    public string Title { get; set; }
+
+    public string? Summary { get; set; }
+
+    public int NumberOfPages { get; set; }
+
+    public string Author { get; set; }
+
+    public string Publisher { get; set; }
+
+    public DateTime PublishedOn { get; set; }
+}
---
+++ LibraryApi/LibraryApi/Controllers/BooksController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class BooksController : ControllerBase
+{
+
+    private readonly ILogger<BooksController> _logger;
+
+    public BooksController(ILogger<BooksController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetBooks")]
+    public IEnumerable<Book> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new Book
+        {
+            Title = "This is a Book",
+            Author = "John Doe",
+            Summary = "This is just a book summary",
+            Publisher = "Fred Publisher",
+            NumberOfPages = 215,
+            PublishedOn = DateTime.Now.AddDays(index),
+        })
+        .ToArray();
+    }
+}
+
