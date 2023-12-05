a6dcd6baee3f0d2cf2b8e7058e958d77637d8ea9
jeiter <eiter.josef@googlemail.com>
2023-12-05 11:24:22 +0100

    adapt swagger

--- LibraryApi/Library.Api.Controllers/Program.cs
+++ LibraryApi/Library.Api.Controllers/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Library.Api.Controllers.Mapping;
 using Library.Core.Application;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,7 +11,16 @@ builder.Services.AddAutoMapper(typeof(MappingProfile));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Library API",
+        Description = "The Library API is a straightforward tool for managing books, patrons, and transactions in library systems. It offers easy integration, simplifying tasks like searching, borrowing, and returning books.",
+        Version = "v1"
+    });
+});
 
 var app = builder.Build();
 
