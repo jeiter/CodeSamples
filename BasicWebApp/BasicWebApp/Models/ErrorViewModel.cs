8742d969252181dc6688024238aee1237f727676
jeiter <eiter.josef@googlemail.com>
2024-01-06 13:28:53 +0100

    add basic web app

---
+++ BasicWebApp/BasicWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BasicWebApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
